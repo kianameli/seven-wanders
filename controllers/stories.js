67ee2a3084586678a7403befb76af134123ea202
Kian Ameli <kianameli@Kians-MBP.attlocal.net>
2021-10-11 10:13:36 -0400

    added controllers for stories; added signUp controller for users

--- controllers/stories.js
+++ controllers/stories.js
@@ -1,7 +1,54 @@
 import Story from "../models/story.js";
 
-export const getStories = async (req, res) => {};
-export const getStory = async (req, res) => {};
-export const createStory = async (req, res) => {};
-export const updateStory = async (req, res) => {};
-export const deleteStory = async (req, res) => {};
+export const getStories = async (req, res) => {
+  try {
+    const stories = await Story.find();
+    res.json(stories);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getStory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const story = await Story.findById(id).populate("userId");
+    if (story) {
+      return res.json(story);
+    }
+    res.status(404).json({ message: "Story not found!" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const createStory = async (req, res) => {
+  try {
+    const story = new Story(req.body);
+    await story.save();
+    res.status(201).json(story);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+export const updateStory = async (req, res) => {
+  const { id } = req.params;
+  const story = await Story.findByIdAndUpdate(id, req.body, { new: true });
+  res.status(200).json(story);
+};
+export const deleteStory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Story.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send("story deleted");
+    }
+    throw new Error("story not found");
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
--- controllers/users.js
+++ controllers/users.js
@@ -3,10 +3,42 @@ import jwt from "jsonwebtoken";
 
 import User from "../models/user.js";
 
-const SALT_ROUNDS = process.env.SALT_ROUNDS || 11;
-const TOKEN_KEY = process.env.TOKEN_KEY || "thisisjustatokentokenkey";
+const SALT_ROUNDS =
+  process.env.NODE_ENV === "production" ? process.env.SALT_ROUNDS : 11;
+const TOKEN_KEY =
+  process.env.NODE_ENV === "production"
+    ? process.env.TOKEN_KEY
+    : "thisisjustatokentokenkey";
 
-export const signUp = async (req, res) => {};
+const today = new Date();
+const exp = new Date(today);
+exp.setDate(today.getDate() + 28);
+
+export const signUp = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const password_digest = await bcrypt.hash(password, Number(SALT_ROUNDS));
+    const user = new User({
+      username,
+      email,
+      password_digest,
+    });
+    await user.save();
+    const payload = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      exp: parseInt(exp.getTime() / 1000),
+    };
+    const token = jwt.sign(payload, TOKEN_KEY);
+    res.status(201).json({ token });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
+};
+
 export const signIn = async (req, res) => {};
+
 export const verify = async (req, res) => {};
 //change pw?
