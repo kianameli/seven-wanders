5007d79d34e47aa80555cc8483345ae3407d10a1
Ian-Flaherty <88514194+Ian-Flaherty@users.noreply.github.com>
2021-10-11 09:31:09 -0500

    users.js updated

--- controllers/users.js
+++ controllers/users.js
@@ -38,7 +38,41 @@ export const signUp = async (req, res) => {
   }
 };
 
-export const signIn = async (req, res) => {};
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email: email }).select(
+      'username email password_digest'
+    )
+    if (await bcrypt.compare(password, user.password_digest)) {
+      const payload = {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        exp: parseInt(exp.getTime() / 1000),
+      }
 
-export const verify = async (req, res) => {};
-//change pw?
+      const token = jwt.sign(payload, TOKEN_KEY)
+      res.status(201).json({ token })
+    } else {
+      res.status(401).send('Invalid Credentials')
+    }
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({ error: error.message })
+  }
+};
+
+export const verify = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    const payload = jwt.verify(token, TOKEN_KEY)
+    if (payload) {
+      res.json(payload)
+    }
+  } catch (error) {
+    console.log(error.message)
+    res.status(401).send('Not Authorized')
+  }
+};
+//post MVP change pw?
