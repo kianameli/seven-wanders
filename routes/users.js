beffeaf62e055426872df1486a799ad4c6205b84
Kian Ameli <kianameli@Kians-MBP.attlocal.net>
2021-10-11 14:47:33 -0400

    routes?

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ import * as controllers from "../controllers/users.js";
 
 const router = Router();
 
-router.get("/sign-up", controllers.signUp);
-router.get("/sign-in", controllers.signIn);
+router.post("/sign-up", controllers.signUp);
+router.post("/sign-in", controllers.signIn);
 router.get("/verify", controllers.verify);
 //change password?
 
