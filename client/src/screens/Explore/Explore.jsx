2c8e1bf32ae1328be6443b52df1f0133e9f4ed64
Ian-Flaherty <88514194+Ian-Flaherty@users.noreply.github.com>
2021-10-12 14:11:27 -0500

    explore page and search made

--- client/src/screens/Explore/Explore.jsx
+++ client/src/screens/Explore/Explore.jsx
@@ -1,12 +1,18 @@
 import './Explore.css'
 import Layout from '../../components/Layout/Layout'
+import SectionDivider from '../../components/SectionDivider/SectionDivider'
+import Search from '../../components/Search/Search'
+import ContinentBar from '../../components/ContinentBar/ContinentBar'
 
 export default function Explore() {
   return (
     <Layout>
-      <div>
-        Explore
-      </div>
+      <img src='https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fres.cloudinary.com%2Fdyyjvyqtn%2Fimage%2Fupload%2Fv1634060377%2F33pOL_apxepy.jpg' alt='explore-back' />
+      <h1 className='explore-title'>Explore</h1>
+      <Search />
+      <SectionDivider text='Continents' />
+      <ContinentBar />
+      <SectionDivider text='Stories' />
     </Layout>
   )
 }
