ecc19b3c4dc41c0ae6e1ab693e7b8c5618cee468
Ian-Flaherty <88514194+Ian-Flaherty@users.noreply.github.com>
2021-10-14 12:34:08 -0500

    home page stories added in proper form

--- client/src/components/StoryCard/StoryCard.jsx
+++ client/src/components/StoryCard/StoryCard.jsx
@@ -5,18 +5,23 @@ export default function StoryCard(props) {
   const {_id,title,location,country,imageURL}=props.story
   
   return (
+    
+
     <Link
       to={`/stories/${_id}`}
       className='story-card-container'
-    >
+      >
+      
       <div className='story-card-image-container'>
         <img className='stories-image'
           src={imageURL}
           alt={title}
-        />
+          />
         <p>{title}</p>
         <p>{location}, {country}</p>
       </div>
+      
     </Link>
+          
   )
 }
