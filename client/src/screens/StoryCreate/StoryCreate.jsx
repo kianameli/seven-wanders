1f964b311eaf8f9ddedc19ed284c4d861019a422
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-10-14 15:21:51 -0500

    Edit page styling

--- client/src/screens/StoryCreate/StoryCreate.jsx
+++ client/src/screens/StoryCreate/StoryCreate.jsx
@@ -59,7 +59,7 @@ export default function StoryCreate(props) {
             onChange={handleChange}
           />
 
-          <SectionDivider text='Image URL' />
+          <SectionDivider text='Image Link' />
           <input
             className='create-form-input'
             placeholder='| Post your image link here'
