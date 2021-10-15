6124142ece2d6c8ba3bf27ce7bb993074d63233d
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-10-15 13:59:13 -0500

    styling tweaks

--- client/src/screens/StoryDetail/StoryDetail.jsx
+++ client/src/screens/StoryDetail/StoryDetail.jsx
@@ -1,7 +1,7 @@
 import './StoryDetail.css'
 import Layout from '../../components/Layout/Layout'
 import { getStory } from '../../services/stories'
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import SectionDivider from '../../components/SectionDivider/SectionDivider';
 import { Link } from 'react-router-dom';
@@ -10,33 +10,33 @@ export default function StoryDetail(props) {
   const [theStory, setTheStory] = useState({});
   //keeping _id for postMVP user edit auth
   // eslint-disable-next-line
-  const {_id,title,location,country,continent,author,story,imageURL}=theStory
+  const { _id, title, location, country, continent, author, story, imageURL } = theStory
   const { id } = useParams()
-  
+
   useEffect(() => {
     const fetchStory = async (storyID) => {
       const aStory = await getStory(storyID)
-        setTheStory(aStory)
+      setTheStory(aStory)
     }
     fetchStory(id)
     // eslint-disable-next-line
-  },[])
+  }, [])
 
   return (
     <Layout user={props.user}>
-<div className='edit-page'>
-      <div className='edit-page-details'>
-        
-        <SectionDivider text={`${country}, ${continent}`} />
-        <h1 className='header-h1'>{`${title}`}</h1>
-        <p>by {author}</p>
-        <img src={imageURL} alt={location} />
-        <section className='section-edit'>{story}</section>
-        <div className='button-section'>
-        <Link className='button' to={`/stories/${id}/edit`}><button className='edit-button'>Edit Story</button></Link>
+      <div className='edit-page'>
+        <div className='edit-page-details'>
+
+          <SectionDivider text={`${country}, ${continent}`} />
+          <h1 className='header-h1'>{`${title}`}</h1>
+          <p>by {author}</p>
+          <img className='storydetail-image' src={imageURL} alt={location} />
+          <section className='section-edit'>{story}</section>
+          <div className='button-section'>
+            <Link to={`/stories/${id}/edit`}><button className='edit-button'>Edit Story</button></Link>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-    </Layout> 
+    </Layout>
   )
 }
