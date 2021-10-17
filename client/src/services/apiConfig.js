a7c54f36f49b536613c97e9d5a993792a6b6a09f
Kian Ameli <kianameli@Kians-MBP.attlocal.net>
2021-10-17 18:26:16 -0400

    added api to end of baseURL in apiConfig.js

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,23 +1,27 @@
-import axios from 'axios'
+import axios from "axios";
 
 const getToken = () => {
-    return new Promise(resolve => {
-        resolve(`Bearer ${localStorage.getItem('token') || null}`)
-    })
-}
+  return new Promise((resolve) => {
+    resolve(`Bearer ${localStorage.getItem("token") || null}`);
+  });
+};
 
 const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'production'
-        ? 'https://seven-wanders.herokuapp.com/'
-        : 'http://localhost:3000/api'
-})
-
-api.interceptors.request.use(async function (config) {
-    config.headers['Authorization'] = await getToken()
-    return config
-}, function (error) {
-    console.log('Request error: ', error)
-    return Promise.reject(error)
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://seven-wanders.herokuapp.com/api"
+      : "http://localhost:3000/api",
 });
 
-export default api
+api.interceptors.request.use(
+  async function (config) {
+    config.headers["Authorization"] = await getToken();
+    return config;
+  },
+  function (error) {
+    console.log("Request error: ", error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
